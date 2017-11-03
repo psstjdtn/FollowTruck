639a59a20706e317252a2f2b501514b2e492ca5c
Seong soo Park <psstjdtn1@naver.com>
2017-11-03 14:49:31 +0900

    20171103

--- app/src/main/java/com/shinhan/sspark/followtruck/ComSignUpActivity.java
+++ app/src/main/java/com/shinhan/sspark/followtruck/ComSignUpActivity.java
@@ -1,49 +1,43 @@
 package com.shinhan.sspark.followtruck;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
 
 public class ComSignUpActivity extends AppCompatActivity {
 
-    EditText uid, upw, upwhw, hpno, snsid;
-    Button drkbutton, backbutton;
-    String userKey;
+    EditText name, uid, upw, upwhw, hpno, snsid;
+    RadioButton custom_radio, biz_radio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_com_sign_up);
 
-        //  id초기화
-        uid        = (EditText)findViewById(R.id.uid);
-        upw        = (EditText)findViewById(R.id.upw);
-        upwhw      = (EditText)findViewById(R.id.upwhw);
-        hpno       = (EditText)findViewById(R.id.hpno);
-        snsid      = (EditText)findViewById(R.id.snsid);
+        Button backbutton = (Button)findViewById(R.id.backbutton);
 
-        drkbutton  = (Button)findViewById(R.id.drkbutton);
-        backbutton = (Button)findViewById(R.id.backbutton);
-
-        Intent intent = getIntent(); // 이 액티비티를 부른 인텐트를 받는다.
-        userKey       = intent.getStringExtra("USER_KEY"); // 로그인인지 회원가입인지 넘겨온다
-
-        // 회원가입을 어디서 호출된 것에 따른 버튼 색깔 변화
-        if (userKey.equals("BIZ")){
-            drkbutton.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.biz_btn_bg));
-            backbutton.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.biz_btn_bg));
-        }
-        else
-        if (userKey.equals("CUS")) {
-            drkbutton.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.cus_btn_bg));
-            backbutton.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.cus_btn_bg));
-        }
-
-        // 등록 버튼이벤트 활성화
-        drkbutton.setOnClickListener(new View.OnClickListener(){
+        // 뒤로가기 버튼이벤트 활성화
+        backbutton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent intent = new Intent(ComSignUpActivity.this, SubActivity.class);
                 // 페이지 넘어가기
@@ -52,13 +46,124 @@ public class ComSignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
-        // 뒤로가기 버튼이벤트 활성화
-        backbutton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                // 뒤로가기
-                finish();
-            }
-        });
+    public void signup(View view) {
+        //  id초기화
+        name       = (EditText)findViewById(R.id.name);
+        uid        = (EditText)findViewById(R.id.uid);
+        upw        = (EditText)findViewById(R.id.upw);
+        upwhw      = (EditText)findViewById(R.id.upwhw);
+        hpno       = (EditText)findViewById(R.id.hpno);
+        snsid      = (EditText)findViewById(R.id.snsid);
+
+        custom_radio = (RadioButton)findViewById(R.id.custom_radio);
+        biz_radio    = (RadioButton)findViewById(R.id.biz_radio);
+
+
+        // req.body.userid, req.body.name, req.body.password, req.body.hpno, req.body.snsid, req.body.user_code
+        new SignUp().execute(
+                "http://172.16.2.3:52273/users",
+                uid.getText().toString(),
+                name.getText().toString(),
+                upw.getText().toString(),
+                hpno.getText().toString(),
+                snsid.getText().toString(),
+                "1"
+                );
+    }
+    class SignUp extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(ComSignUpActivity.this);
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+
+                // req.body.userid, req.body.name, req.body.password, req.body.hpno, req.body.snsid, req.body.user_code
+                postDataParams.put("userid", params[1]);
+                postDataParams.put("name", params[2]);
+                postDataParams.put("password", params[3]);
+                postDataParams.put("hpno", params[4]);
+                postDataParams.put("snsid", params[5]);
+                postDataParams.put("user_code", params[6]);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("회원등록 중...");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+                if (json.getBoolean("result") == true) { //로그인 실패
+                    Toast.makeText(ComSignUpActivity.this,
+                            "회원가입에 성공하였습니다.",
+                            Toast.LENGTH_SHORT).show();
+                    // 끝내기
+                    finish();
+                } else {//로그인 성공
+                    Toast.makeText(ComSignUpActivity.this,
+                            "회원가입에 실패하였습니다. 입력한 내용을 확인해주세요.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
     }
 }
--- app/src/main/java/com/shinhan/sspark/followtruck/SubActivity.java
+++ app/src/main/java/com/shinhan/sspark/followtruck/SubActivity.java
@@ -1,16 +1,32 @@
 package com.shinhan.sspark.followtruck;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 
 public class SubActivity extends AppCompatActivity {
 
     EditText uid, upw;
-    Button login, signup;
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +36,121 @@ public class SubActivity extends AppCompatActivity {
         //  id초기화
         uid    = (EditText)findViewById(R.id.uid);
         upw    = (EditText)findViewById(R.id.upw);
-        login  = (Button)findViewById(R.id.login);
         signup = (Button)findViewById(R.id.signup);
-
-        // 로그인 버튼이벤트 활성화
-        login.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Intent intent = new Intent(SubActivity.this, ComSelActivity.class);
-                intent.putExtra("TRX_G","LOGIN");
-                // 페이지 넘어가기
-                startActivity(intent);
-            }
-        });
 
         // 회원가입 버튼이벤트 활성화
         signup.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                Intent intent = new Intent(SubActivity.this, ComSelActivity.class);
-                intent.putExtra("TRX_G","SIGNUP");
+                Intent intent = new Intent(SubActivity.this, ComSignUpActivity.class);
                 // 페이지 넘어가기
                 startActivity(intent);
             }
         });
 
+    }
+
+    public void login(View view) {
+        //  id초기화
+        uid        = (EditText)findViewById(R.id.uid);
+        upw        = (EditText)findViewById(R.id.upw);
+        // req.body.userid, req.body.name, req.body.password, req.body.hpno, req.body.snsid, req.body.user_code
+        new login().execute(
+                "http://172.16.2.3:52273/users/login",
+                uid.getText().toString(),
+                upw.getText().toString()
+        );
+    }
+    class login extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(SubActivity.this);
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+
+                // req.body.userid, req.body.password
+                postDataParams.put("userid", params[1]);
+                postDataParams.put("password", params[2]);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("로그인 중...");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+                if (json.getBoolean("result") == true) {//로그인 성공
+                    String token = json.getString("token");
+                    SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("token", token);
+                    editor.commit();
+                    Intent intent = new Intent(SubActivity.this, BizManageActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {//로그인 실패
+                    Toast.makeText(SubActivity.this,
+                            "아이디가 없거나 암호가 틀렸습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
     }
 }
